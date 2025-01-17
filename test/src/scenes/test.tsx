4e47a04b9dcb0a1d988ef4d1ea02ce17c621412d
sysraccoon <sysraccoon@gmail.com>
2025-01-17 11:44:43 +0300

    remove test project directory

--- test/src/scenes/test.tsx
+++
@@ -1,6 +0,0 @@
-import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import {waitFor} from '@motion-canvas/core/lib/flow';
-
-export default makeScene2D(function* () {
-  yield* waitFor(5);
-});
