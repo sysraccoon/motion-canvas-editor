bb7843e1668090f1d6cd64d932ea7ec2bba13465
sysraccoon <sysraccoon@gmail.com>
2025-01-20 15:49:52 +0300

    migrate to new eslint config format

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsdoc from 'eslint-plugin-tsdoc';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      '**/*.js',
+      '**/*.d.ts',
+      'packages/template',
+      'packages/create/template-*',
+      '**/lib',
+      '**/node_modules',
+      '**/dist',
+    ],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      tsdoc,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    rules: {
+      'require-yield': 'off',
+      '@typescript-eslint/explicit-member-accessibility': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'tsdoc/syntax': 'warn',
+    },
+  },
+];
